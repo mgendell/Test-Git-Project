d95ea5345aa4d246262ebd02af83142b6a041483
Martin Gendell <martin@gendell.com>
2015-02-21 16:06:20 -0500

    added test class

---
+++ MAGTest.h
@@ -0,0 +1,13 @@
+//
+//  MAGTest.h
+//  Git Test
+//
+//  Created by Martin Gendell on 2/21/15.
+//  Copyright (c) 2015 MAGnetic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MAGTest : NSObject
+
+@end
---
+++ MAGTest.m
@@ -0,0 +1,13 @@
+//
+//  MAGTest.m
+//  Git Test
+//
+//  Created by Martin Gendell on 2/21/15.
+//  Copyright (c) 2015 MAGnetic. All rights reserved.
+//
+
+#import "MAGTest.h"
+
+@implementation MAGTest
+
+@end
