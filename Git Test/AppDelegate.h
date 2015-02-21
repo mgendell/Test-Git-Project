b86a45c0b94c020fe6c32836ac38afe87311f26d
Martin Gendell <martin@Martins-MacBook-Pro.local>
2015-02-21 15:36:15 -0500

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Martin Gendell on 2/21/15.
+//  Copyright (c) 2015 MAGnetic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
